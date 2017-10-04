48702291a7c5ff65ff1ba84599684d2e6a7455a8
Adam Karty <akarty328@gmail.com>
2017-10-04 13:59:35 -0400

    Javascript Exercises

---
+++ script.js
@@ -0,0 +1,23 @@
+var number = prompt('Enter a number');
+for (var i = 0; i++) {
+    console.log(number + i);
+}
+
+var play = prompt('Do you want to play?');
+do {var word = prompt("Enter a Word");
+console.log(word);
+}
+while play = "yes";
+// couldnt figure out how to add the words together
+
+var greeting = prompt('Would you like to print your name?')
+var name = 'Hello my name is Adam';
+while (greeting = 'yes') {
+  console.log(name + '!'++);
+}
+
+var greeting = prompt(What time of the day is it?)
+var morning = 'Since it is morning, you should be eating breakfast. We suggest eggs and toast.'
+var afternoon = 'Since it is afternoon, you should be eating lunch. We suggest a salad'
+var evening = 'Since is it evening, you should be eating dinner. We suggest chicken and rice.'
+console.log(greeting);
